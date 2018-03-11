9ac715b5d6aaf80e5f0ad4f77f49b7f9fa568811
SergeyGutsulyak <kontrolnaia-su@tut.by>
2018-03-11 12:15:36 +0300

    ДЗ N17 выполнено версия 1

--- N17/common.js
+++ N17/common.js
@@ -1,45 +1,42 @@
-function culcVowel(strIsh) {
-	vowelSymbol={'А':true,'О':true, 'Е':true, 'Ё':true, 'И':true,
-	             'Ы':true,'Ю':true, 'Я':true, 'Э':true, 'У':true};
-	var sum=0;
-    var strR=strIsh.toUpperCase();
-    
-
-	var symb
-	for (var i=0;i<strR.length;i++){
-		symb=strR[i];
-		if (symb in vowelSymbol){
-			sum++;
-		}
-	}
-	return sum;	
-}
 //для forEach
-function f1(v,i,a){
+function f1(strOriginal){
     vowelSymbol={'А':true,'О':true, 'Е':true, 'Ё':true, 'И':true,
     'Ы':true,'Ю':true, 'Я':true, 'Э':true, 'У':true};
-    if (v in vowelSymbol) return 1;
-    else return 0;
-
+    var sum=0
+    strOriginal.toUpperCase().split('').forEach(function(v){
+        if (v in vowelSymbol) sum++;
+    });
+    return sum;
 }
 //для reduce
-function f2(r,v,i,a){
+function f2(strOriginal){
     vowelSymbol={'А':true,'О':true, 'Е':true, 'Ё':true, 'И':true,
     'Ы':true,'Ю':true, 'Я':true, 'Э':true, 'У':true};
-    if (v in vowelSymbol) return r++;
-    else return r;
+    var sum
+    sum=strOriginal.toUpperCase().split('').reduce(function(r,v){
+        if (v in vowelSymbol) r++;
+        return r;
+    },0);
+    return sum;
 }
 //для filter
-function f3(v,i,a){
+function f3(strOriginal){
     vowelSymbol={'А':true,'О':true, 'Е':true, 'Ё':true, 'И':true,
     'Ы':true,'Ю':true, 'Я':true, 'Э':true, 'У':true};
-
-    if (v in vowelSymbol) return True;
-    else return False;
-
+    var massVowelSymbol=strOriginal.toUpperCase().split('').filter(function(v){
+        if (v in vowelSymbol) return true;
+        else return false;
+    });
+    return massVowelSymbol.length;
 }
 
 str=prompt('Введите строку');
-var rez='В строке '+str+' '+culcVowel(str)+' гласных букв';
+var rez='Расчет с forEach: В строке '+str+' '+f1(str)+' гласных букв';
+console.log(rez);
+alert(rez);
+rez='Расчет с reduce: В строке '+str+' '+f2(str)+' гласных букв';
+console.log(rez);
+alert(rez);
+rez='Расчет с filter: В строке '+str+' '+f3(str)+' гласных букв';
 console.log(rez);
 alert(rez);
