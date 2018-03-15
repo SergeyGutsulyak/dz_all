ce532c844e7c080d847534488fdb5685afd7a06f
SergeyGutsulyak <kontrolnaia-su@tut.by>
2018-03-15 23:19:22 +0300

    класc HashStorage

--- N13/HashStorage.js
+++ N13/HashStorage.js
@@ -12,20 +12,24 @@ HashStorage.prototype.getValue=function(key){
 }
 
 HashStorage.prototype.deleteValue=function(key){
-    
+    delete this.data[key]
 }
 
 HashStorage.prototype.getKeys=function(){
-    
+    var keys=[]
+    for (el in this.data){
+        keys.push(el);
+    }
+    return keys;
 }
 
-var a = new HashStorage;
-var c=[1,2,3,4,5,6,7]
-var b={1:2,3:4,5:c}
-console.log(b)
-a.addValue(7,b)
-console.log(a.data)
-b[9]=10
-c.
-console.log(a.data)
+//var a = new HashStorage;
+//var b={1:2,3:4}
+//console.log(b)
+//a.addValue(7,b)
+//a.addValue(5,6)
+//a.addValue(7,8)
+//console.log(a.data)
+//console.log(a.getKeys())
+//console.log(a.data)
 
